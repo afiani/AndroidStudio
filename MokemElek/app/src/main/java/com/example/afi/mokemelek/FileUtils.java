cfc8216e9828b942802364161dc3d622d52bf797
afiani <astiafiani@gmail.com>
2017-01-19 01:47:43 +0700

    MokemElek
    MokemElek Projek

---
+++ MokemElek/app/src/main/java/com/example/afi/mokemelek/FileUtils.java
@@ -0,0 +1,18 @@
+package com.example.afi.mokemelek;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Created by Afi on 12/5/2016.
+ */
+public class FileUtils {
+
+    public static int readLines(File todoFile) {
+
+        return 0;
+    }
+
+    public static void writeLines(File todoFile, ArrayList<String> items) {
+    }
+}
